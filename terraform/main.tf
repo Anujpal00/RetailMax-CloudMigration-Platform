bdc6464522622d5bd7ccaec9a51b9b1fc0db54db
kartik <rekartikchauhan@gmail.com>
2025-07-28 16:34:20 +0530

    terraform updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -96,8 +96,8 @@ resource "aws_security_group" "web_sg" {
 
 resource "aws_instance" "web" {
   ami           = "ami-020cba7c55df1f615" 
-  instance_type = "t2.micro"
-  key_name      = "Demo" 
+  instance_type = "t3..micro"
+  key_name      = "intern" 
   subnet_id     = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
